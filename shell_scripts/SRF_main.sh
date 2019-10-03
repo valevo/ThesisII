8690c0b25ae9479c2b877a441fd71a152ec83d46
valevo <valevogelmann@gmail.com>
2019-10-03 19:56:17 +0200

    uploading shell script

--- shell_scripts/SRF_main.sh
+++ shell_scripts/SRF_main.sh
@@ -8,8 +8,26 @@ module load Python/3.6.1-intel-2016b
 
 echo "Stat job $PBS_JOBID started at `date`"
 
-mkdir "$TMPDIR"/Thesis
 
-cp -r $HOME/Thesis/data "$TMPDIR"/Thesis
+lang=FI
+
+#mkdir "$TMPDIR"/ThesisII
+#mkdir "$TMPDIR"/ThesisII/data
+#cp -r $HOME/ThesisII/data/"$lang" "$TMPDIR"/ThesisII/data
+#cp -r $HOME/ThesisII/filtering "$TMPDIR"/ThesisII
+#cp -r $HOME/ThesisII/stats "$TMPDIR"/ThesisII
 
 
+rsync -v --progress $HOME/ThesisII "$TMPDIR"/ --exclude data
+cd "$TMPDIR"/ThesisII
+mkdir data
+cp -r $HOME/ThesisII/data/"$lang" "$TMPDIR"/ThesisII/data/
+
+
+python3.6 SRF_main.py --lang=$lang --n_tokens=1e6 --hist_len=2
+
+
+cp -r $TMPDIR/ThesisII/results/"$lang"/ $HOME/ThesisII/results
+
+
+echo "Job $PBS_JOBID ended at `date`"
