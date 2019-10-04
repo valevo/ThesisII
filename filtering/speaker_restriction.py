0d377579d7ad729f2b8f169248ffd92c6352a204
valevo <valevogelmann@gmail.com>
2019-10-04 09:25:37 +0200

    testing...

--- filtering/speaker_restriction.py
+++ filtering/speaker_restriction.py
@@ -32,10 +32,14 @@ def filter_speaker_restrict(sents, n, history_len):
         if np.intersect1d(sampled_s, cur_disallowed).size > 0:
             continue
         
+        print("\n\n", flush=True)
+        print("SAMPLED_S", sampled_s, flush=True)
+        print("\n\n", flush=True)
         cur_hist.append(sampled_s)
         if len(cur_hist) > history_len:
             cur_hist.pop(0)
         print("CUR_HIST", cur_hist, flush=True)
+        print("\n\n", flush=True)
         
         used.add(cur_sample)
         sampled += len(sampled_s)
