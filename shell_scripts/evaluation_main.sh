f715c03cb73c1d42b00164e3040686e745c2ae37
valevo <valevogelmann@gmail.com>
2019-11-11 15:23:53 +0100

    evaluation

--- shell_scripts/evaluation_main.sh
+++ shell_scripts/evaluation_main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 3:00:00
+#SBATCH -t 12:00:00
 #SBATCH --mem=25G
 
 module load pre2019
@@ -26,20 +26,29 @@ echo "language: $lang"
 
 cp -r $HOME/ThesisII/data/"$lang"_pkl "$TMPDIR"/ThesisII/data/
 
-python3 typicality_eval.py --lang=$lang --factors 2 6 10 14 18 22 --hist_lens 2 4 8 16 32 64 81
 
-echo
-echo "done with typicality evaluation at `date`"
 
 python3 normality_eval.py --lang=$lang --factors 2 6 10 14 18 22 --hist_lens 2 4 8 16 32 64 81 
 
 echo
-echo "done with language $lang at `date`"
+echo "done with normality evaluation at `date`"
 
 cp -r $TMPDIR/ThesisII/results/$lang/evaluation $HOME/ThesisII/results/$lang/
 
 echo "and copied"
 echo
+
+
+python3 typicality_eval.py --lang=$lang --factors 2 6 10 14 18 22 --hist_lens 2 4 8 16 32 64 81
+
+echo
+echo "done with typicality evaluation at `date`"
+
+cp -r $TMPDIR/ThesisII/results/$lang/evaluation $HOME/ThesisII/results/$lang/
+
+echo "and copied"
+echo
+
 
 
 echo "Job $PBS_JOBID ended at `date`"
